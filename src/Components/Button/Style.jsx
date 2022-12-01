e0181d409169085991aa7913eb62471d45e8c6d2
Lucas Lafèré <laferelucas@gmail.com>
2022-12-01 17:05:22 -0300

    feat: added button border

--- src/Components/Button/Style.jsx
+++ src/Components/Button/Style.jsx
@@ -2,12 +2,13 @@ import styled from 'styled-components';
 
 export const Button = styled.div`
   background-color: ${(props) => props.backgroundColor};
-  border: none;
+  border: 1px solid #000;
+  border-radius: 10px;
   padding: ${({ padding }) => padding};
   width: ${({ size }) =>
-    (size === 'small' && '100px') || (size === 'large' && '300px') || (size === 'medium' && '200px')};
+    (size === 'small' && '100px') || (size === 'medium' && '140px') || (size === 'large' && '180px')};
   height: ${({ size }) =>
-    (size === 'small' && '100px') || (size === 'large' && '300px') || (size === 'medium' && '200px')};
+    (size === 'small' && '40px') || (size === 'medium' && '60px') || (size === 'large' && '80px')};
   display: flex;
   align-items: center;
   justify-content: center;
