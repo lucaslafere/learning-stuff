32881624a3f74f8bc8f1ef73e2f0c0aacba93612
Lucas Lafèré <laferelucas@gmail.com>
2022-12-08 15:05:59 -0300

    testing style.js or .jsx

---
+++ src/Pages/TestComponent/Test.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect, useContext } from "react";
+import styled from "styled-components";
+
+export default function Test() {
+  return <Container></Container>;
+}
+
+const Container = styled.div``;
