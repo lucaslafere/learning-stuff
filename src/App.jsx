5b7c58c78011ea1c5b1ed104db5fffd80b5bb43c
Lucas Lafèré <laferelucas@gmail.com>
2022-12-08 14:59:07 -0300

    linting

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import { useState } from "react";
+
+export default function App() {
+  const [estado, setEstado] = useState("estadinho");
+
+  return (
+    <>
+      <Container onClick={() => setEstado("kkkkkk mudei")}>
+        oiiii, esse é o meu estado: {estado}, mas se vc clicar aqui eu vou mudar
+        de nome
+      </Container>
+    </>
+  );
+}
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+  color: red;
+  background-color: #f5f5f5;
+`;
